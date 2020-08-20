b3a2dca352d9a92edb9afb90feccd73984d8e1b6
a2oL <66704377+a2oL@users.noreply.github.com>
2020-08-20 13:26:57 -0500

    eventos desde json

--- app/src/main/java/mx/org/ieem/adapters/EventosAdapter.java
+++ app/src/main/java/mx/org/ieem/adapters/EventosAdapter.java
@@ -1,22 +1,32 @@
 package mx.org.ieem.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import mx.org.ieem.R;
+import mx.org.ieem.data.sqllite.models.eventos.eventos;
 
 import static mx.org.ieem.R.layout.list_eventos;
+import static mx.org.ieem.activity.eventos.EventosActivity.listapersonas;
 
 public class EventosAdapter extends BaseAdapter
 {
     Context context;                                                    // Contiene el contexto de PreguntasEncuestaActivity.
-    LayoutInflater inflter;                                             // Utilizado para poder cargar elementos de la ui.
+    LayoutInflater inflter;
+    AppCompatActivity appCompatActivity;                    // Utilizado para poder cargar elementos de la ui.
 
 
     public EventosAdapter(Context context) {
@@ -26,7 +36,7 @@ public class EventosAdapter extends BaseAdapter
 
     @Override
     public int getCount() {
-        return 0;
+        return listapersonas.size();
     }
 
     @Override
@@ -42,11 +52,17 @@ public class EventosAdapter extends BaseAdapter
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        convertView = inflter.inflate(list_eventos, null);
-
-        Drawable imageNino = ContextCompat.getDrawable(context.getApplicationContext(), R.drawable.btn_nino);                   // Contiene la imagen ninio Azul
-        Drawable imageNina = ContextCompat.getDrawable(context.getApplicationContext(), R.drawable.btn_nina);                   // Contiene la imagen ninia rosa.
-        Drawable imageOtra = ContextCompat.getDrawable(context.getApplicationContext(), R.drawable.btn_nino_nina);
-        return null;
+        View view = inflter.inflate(R.layout.list_eventos, null);
+        eventos ev = listapersonas.get(position);
+        ImageView imagen = (ImageView) view.findViewById(R.id.imageViewEvento);
+        TextView texto = (TextView) view.findViewById(R.id.textViewEvento);
+        String base64String = ev.getImage();
+        String base64Image = base64String.split(",")[1];
+        byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        Drawable drawable = new BitmapDrawable(context.getResources(), decodedByte);
+        imagen.setImageDrawable(drawable);
+        texto.setText(ev.getNombre());
+        return view;
     }
 }
