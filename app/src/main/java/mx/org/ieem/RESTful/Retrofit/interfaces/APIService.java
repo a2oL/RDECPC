2bc8291c68c606ca7e13ac6b958a7b445d8bf87f
a2oL <66704377+a2oL@users.noreply.github.com>
2020-09-14 12:21:16 -0500

    Reportes  y Eventos..

---
+++ app/src/main/java/mx/org/ieem/RESTful/Retrofit/interfaces/APIService.java
@@ -0,0 +1,35 @@
+package mx.org.ieem.RESTful.Retrofit.interfaces;
+
+import java.util.List;
+
+import mx.org.ieem.RESTful.JSONModels.PostCiudadanometro.Ciudadanometro;
+import mx.org.ieem.RESTful.JSONModels.PostEncuestas.EncuestasJuvenile;
+import mx.org.ieem.RESTful.JSONModels.ResponseLogin.ResponseLogin;
+import mx.org.ieem.RESTful.JSONModels.UsuarioJM;
+import mx.org.ieem.data.sqllite.models.eventos.trdd_concurso_o_evento;
+import mx.org.ieem.data.sqllite.models.reportes.trdd_reporte;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @POST("/redDigitalDpc/login")
+    Call<ResponseLogin> createUser(@Body UsuarioJM userJM);
+
+    @Headers("Content-Type: application/json")
+    @POST("/redDigitalDpc/encuesta-juvenil/encuestas")
+    Call<List<EncuestasJuvenile>> insertEncuestas(@Body List<EncuestasJuvenile> sendEncuestas);
+
+    @Headers("Content-Type: application/json")
+    @POST("/redDigitalDpc/ciudadanometro/encuestas")
+    Call<List<Ciudadanometro>> insertCiudadanometros(@Body List<Ciudadanometro> sendCiudadano);
+
+    @Headers("Content-Type: application/json")
+    @POST("/redDigitalDpc/eventos_y_concursos_vigentes")
+    Call<List<trdd_concurso_o_evento>> getEventos();
+
+    @Headers("Content-Type: application/json")
+    @POST("/redDigitalDpc/reportes")
+    Call<List<trdd_reporte>> getReportes();
+}
